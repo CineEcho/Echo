bb3e851960645006c25d1e47811755547a921a66
Pixar <128686206+CineEcho@users.noreply.github.com>
2025-05-09 23:29:06 +0800

    create

---
+++ scripts/fetch-movies.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const axios = require('axios');
+const fs = require('fs');
+const API_KEY = process.env.TMDB_API_KEY;
+// 请求 URL
+const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}&language=en-US`;
+
+// 请求 TMDB API 获取电影数据
+axios.get(url)
+  .then(response => {
+    // 将数据保存到 JSON 文件
+    fs.writeFileSync('./data/movies.json', JSON.stringify(response.data, null, 2));
+    console.log('电影数据已保存到 movies.json');
+  })
+  .catch(error => {
+    console.error('抓取失败:', error);
+  });
